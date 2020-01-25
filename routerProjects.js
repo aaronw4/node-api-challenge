2c9eebf633be511e744b17e5f75bf38350b5decb
Aaron Williams <aaronrw4@yahoo.com>
2020-01-24 19:02:52 -0800

    get projects by id works

--- routerProjects.js
+++ routerProjects.js
@@ -14,4 +14,24 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/:id', (req, res) => {
+    const {id} = req.params;
+
+    db.get(id)
+        .then(project => {
+            if (!project) {
+                res.status(404).json({
+                    error: 'Invalid id'
+                })
+            } else {
+                res.status(200).json(project)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: 'The project could not be retrieved.'
+            })
+        })
+})
+
 module.exports = router;
