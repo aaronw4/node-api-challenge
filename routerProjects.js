e99a988b8a4197c24c5ce6a0520db95ef72d29e4
Aaron Williams <aaronrw4@yahoo.com>
2020-01-24 20:07:58 -0800

    projectActions works

--- routerProjects.js
+++ routerProjects.js
@@ -98,4 +98,22 @@ router.put('/:id', (req, res) => {
      })
 })
 
+router.get('/projectActions/:id', (req, res) => {
+    db.getProjectActions(req.params.id)
+    .then(project => {
+        if (!project) {
+            res.status(404).json({
+                error: 'Invalid id'
+            })
+        } else {
+            res.status(200).json(project)
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: 'The project could not be retrieved.'
+        })
+    })
+})
+
 module.exports = router;
