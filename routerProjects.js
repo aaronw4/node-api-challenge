8b27b19acbaa4fb7aacfa05b02113a9379a459c9
Aaron Williams <aaronrw4@yahoo.com>
2020-01-24 19:17:14 -0800

    post project works

--- routerProjects.js
+++ routerProjects.js
@@ -32,6 +32,26 @@ router.get('/:id', (req, res) => {
                 error: 'The project could not be retrieved.'
             })
         })
+});
+
+router.post('/', (req, res) => {
+    const {name, description} = req.body;
+
+    if (!name || !description) {
+        res.status(400).json({
+            error: 'Please provide name, description, and completed for the post.'
+        })
+    }
+
+    db.insert(req.body)
+     .then(project => {
+         res.status(201).json(project)
+     })
+     .catch(err => {
+         res.status(500).json({
+             error: 'There was an error while saving the post to the database'
+         })
+     })
 })
 
 module.exports = router;
