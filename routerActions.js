8bc97f23b20b064ccfa2c7fe4b73fb06b0fdd43d
Aaron Williams <aaronrw4@yahoo.com>
2020-01-24 20:01:21 -0800

    put action works

--- routerActions.js
+++ routerActions.js
@@ -72,4 +72,30 @@ router.delete('/:id', (req, res) => {
         })
 })
 
+router.put('/:id', (req, res) => {
+    const {project_id, description, notes} = req.body;
+
+    if (!project_id || !description || !notes) {
+        res.status(400).json({
+            error: 'Please provide project_id, description, and notes for the post.'
+        })
+    }
+
+    db.update(req.params.id, req.body)
+     .then(action => {
+         if (action) {
+            res.status(200).json(action)
+         } else {
+             res.status(404).json({
+                 error: 'The action with specified id does not exist.'
+             })
+         }         
+     })
+     .catch(err => {
+         res.status(500).json({
+             error: 'The action informaton could not be modified.'
+         })
+     })
+})
+
 module.exports = router;
