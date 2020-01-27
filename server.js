9f5e0578e8526a100ed69126f25028a0bd44a48e
Aaron Williams <aaronrw4@yahoo.com>
2020-01-26 22:19:14 -0800

    Installed cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const routerProjects = require('./routerProjects');
 const routerActions = require('./routerActions');
+const cors = require('cors');
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 
 server.use('/projects', routerProjects);
 server.use('/actions', routerActions);
